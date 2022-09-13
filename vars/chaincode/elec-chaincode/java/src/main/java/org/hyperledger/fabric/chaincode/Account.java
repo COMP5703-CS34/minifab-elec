9d627e9da350f4ac71a397c89d440967517d007d
Evan Xu <tzxuyiwei100@126.com>
2022-09-13 19:27:44 +0800

    CC: initial ELEC ChainCode Update

---
+++ vars/chaincode/elec-chaincode/java/src/main/java/org/hyperledger/fabric/chaincode/Account.java
@@ -0,0 +1,35 @@
+package org.hyperledger.fabric.chaincode;
+
+public class Account {
+    private String accountId;
+    private Double elecAmount;
+    private Double balance;
+    
+    public Account(String accountId, Double elecAmount, Double balance) {
+        this.accountId = accountId;
+        this.elecAmount = elecAmount;
+        this.balance = balance;
+    }
+
+    private Account() {}
+
+    public String getAccountId(
+        return accountId;
+    )
+
+    public Double getElecAmount() {
+        return elecAmount;
+    }
+
+    public Double getElecAmount() {
+        return elecAmount;
+    }
+
+    public void setElecAmount(String elecAmount) {
+        this.elecAmount = elecAmount;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+}
--- vars/chaincode/elec-chaincode/java/src/main/java/org/hyperledger/fabric/chaincode/AccountBasedChaincode.java
+++ vars/chaincode/elec-chaincode/java/src/main/java/org/hyperledger/fabric/chaincode/AccountBasedChaincode.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.shim.ChaincodeBase;
 import org.hyperledger.fabric.shim.ChaincodeStub;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -18,24 +19,32 @@ public class AccountBasedChaincode extends ChaincodeBase {
     @Override
     public Response init(ChaincodeStub stub) {
         try {
-            _logger.info("Init java simple chaincode");
+            _logger.info("Init elec-transaction java chaincode");
             String func = stub.getFunction();
             if (!func.equals("init")) {
                 return newErrorResponse("function other than init is not supported");
             }
             List<String> args = stub.getParameters();
-            if (args.size() != 4) {
-                newErrorResponse("Incorrect number of arguments. Expecting 4");
+            if (args.size() != 6) {
+                newErrorResponse("Incorrect number of arguments. Expecting 6");
             }
             // Initialize the chaincode
             String account1Key = args.get(0);
-            int account1Value = Integer.parseInt(args.get(1));
-            String account2Key = args.get(2);
-            int account2Value = Integer.parseInt(args.get(3));
+            int account1ElecAmount = Integer.parseInt(args.get(1));
+            int account1Balance = Integer.parseInt(args.get(2));
+            Account account1 = Account(account1Key, account1ElecAmount, account1Balance);
 
-            _logger.info(String.format("account %s, value = %s; account %s, value %s", account1Key, account1Value, account2Key, account2Value));
-            stub.putStringState(account1Key, args.get(1));
-            stub.putStringState(account2Key, args.get(3));
+            String account2Key = args.get(3);
+            int account2ElecAmount = Integer.parseInt(args.get(4));
+            int account2Balance = Integer.parseInt(args.get(5));
+            Account account2 = Account(account2Key, account2ElecAmount, account2Balance);
+
+            _logger.info(String.format("account %s, elec = %d, balance = %d; account %s, elec = %d, balance = %d", 
+                                account1Key, account1ElecAmount, account1Balance, 
+                                account2Key, account2ElecAmount, account2Balance));
+
+            stub.putState(account1Key, (new ObjectMapper()).writeValueAsBytes(account1));
+            stub.putState(account2Key, (new ObjectMapper()).writeValueAsBytes(account2));
 
             return newSuccessResponse();
         } catch (Throwable e) {
@@ -49,8 +58,8 @@ public class AccountBasedChaincode extends ChaincodeBase {
             _logger.info("Invoke java simple chaincode");
             String func = stub.getFunction();
             List<String> params = stub.getParameters();
-            if (func.equals("invoke")) {
-                return invoke(stub, params);
+            if (func.equals("transfer")) {
+                return transfer(stub, params);
             }
             if (func.equals("delete")) {
                 return delete(stub, params);
@@ -64,46 +73,59 @@ public class AccountBasedChaincode extends ChaincodeBase {
         }
     }
 
-    private Response invoke(ChaincodeStub stub, List<String> args) {
+    // Make a Trasfer
+    // Transfer format: {accountFrom, accountTo, transferredAmount, elecPrice}
+    private Response transfer(ChaincodeStub stub, List<String> args) {
         if (args.size() != 3) {
-            return newErrorResponse("Incorrect number of arguments. Expecting 3");
+            return newErrorResponse("Incorrect number of arguments. Expecting 4");
         }
-        String accountFromKey = args.get(0);
-        String accountToKey = args.get(1);
+        // Parse parameters
+        String fromAccountKey = args.get(0);
+        String toAccountKey = args.get(1);
+        int transferredAmount = Integer.parseInt(args.get(2);
+        int elecPrice = Integer.parseInt(args.get(3));
 
-        String accountFromValueStr = stub.getStringState(accountFromKey);
-        if (accountFromValueStr == null) {
+        String fromAccountStr = stub.getStringState(fromAccountKey);
+        if (fromAccountStr == null) {
             return newErrorResponse(String.format("Entity %s not found", accountFromKey));
         }
-        int accountFromValue = Integer.parseInt(accountFromValueStr);
 
-        String accountToValueStr = stub.getStringState(accountToKey);
-        if (accountToValueStr == null) {
+        String toAccountStr = stub.getStringState(toAccountKey);
+        if (toAccountStr == null) {
             return newErrorResponse(String.format("Entity %s not found", accountToKey));
         }
-        int accountToValue = Integer.parseInt(accountToValueStr);
 
-        int amount = Integer.parseInt(args.get(2));
+        ObjectMapper mapper = new ObjectMapper();
+        Account fromAccount = mapper.readValue(fromAccountStr, Account.class);
+        Account toAccount = mapper.readValue(toAccountStr, Account.class);
 
-        if (amount > accountFromValue) {
+        int fromAccountElecAmount = fromAccount.elecAmount;
+        int fromAccountBalance = fromAccount.balance;
+        int toAccountElecAmount = toAccount.elecAmount;
+        int toAccountBalance = toAccount.balance;
+
+        if (transferredAmount * elecPrice > fromAccountBalance) {
             return newErrorResponse(String.format("not enough money in account %s", accountFromKey));
         }
 
-        accountFromValue -= amount;
-        accountToValue += amount;
+        fromAccountElecAmount -= transferredAmount;
+        toAccountElecAmount += transferredAmount;
+        fromAccountBalance += transferredAmount * elecPrice;
+        toAccountBalance -= transferredAmount * elecPrice;
 
-        _logger.info(String.format("new value of A: %s", accountFromValue));
-        _logger.info(String.format("new value of B: %s", accountToValue));
+        _logger.info(String.format("new status of %s: %d %d", fromAccountKey, fromAccountElecAmount, fromAccountBalance));
+        _logger.info(String.format("new status of %s: %d %d", toAccountKey, toAccountElecAmount, toAccountBalance));
 
-        stub.putStringState(accountFromKey, Integer.toString(accountFromValue));
-        stub.putStringState(accountToKey, Integer.toString(accountToValue));
+        stub.putState(fromAccountKey, mapper.writeValueAsBytes(fromAccount));
+        stub.putState(toAccountKey, mapper.writeValueAsBytes(toAccount));
 
         _logger.info("Transfer complete");
 
-        return newSuccessResponse("invoke finished successfully", ByteString.copyFrom(accountFromKey + ": " + accountFromValue + " " + accountToKey + ": " + accountToValue, UTF_8).toByteArray());
+        return newSuccessResponse("transfer finished successfully", ByteString.copyFrom(accountFromKey + ": " + accountFromValue + " " + accountToKey + ": " + accountToValue, UTF_8).toByteArray());
     }
 
     // Deletes an entity from state
+    // Delete format: {deleteAccount}
     private Response delete(ChaincodeStub stub, List<String> args) {
         if (args.size() != 1) {
             return newErrorResponse("Incorrect number of arguments. Expecting 1");
@@ -114,19 +136,23 @@ public class AccountBasedChaincode extends ChaincodeBase {
         return newSuccessResponse();
     }
 
-    // query callback representing the query of a chaincode
+    // Query callback representing the query of a chaincode
+    // Query format: {queryID}
     private Response query(ChaincodeStub stub, List<String> args) {
         if (args.size() != 1) {
             return newErrorResponse("Incorrect number of arguments. Expecting name of the person to query");
         }
         String key = args.get(0);
-        //byte[] stateBytes
-        String val	= stub.getStringState(key);
+        String accountStr	= stub.getStringState(key);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(accountStr, Account.class);
+        
         if (val == null) {
             return newErrorResponse(String.format("Error: state for %s is null", key));
         }
-        _logger.info(String.format("Query Response:\nName: %s, Amount: %s\n", key, val));
-        return newSuccessResponse(val, ByteString.copyFrom(val, UTF_8).toByteArray());
+        _logger.info(String.format("Query Response:\nName: %s, Amount: %d, Balance\n", key, account.elecAmount, acconut.balance));
+        return newSuccessResponse();
     }
 
     public static void main(String[] args) {
